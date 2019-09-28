0ff65be025f2a1f7e4b55010d00a86e40b2e6c5f
Garrett Young <garrettyoung510@gmail.com>
2019-09-27 20:00:59 -0700

    removed date fns dependencies for now due to errors with github

--- scripts/ui.js
+++ scripts/ui.js
@@ -9,9 +9,10 @@ class ChatUI {
     this.list.innerHTML = "";
   }
   render(data) {
-    const when = dateFns.distanceInWordsToNow(data.created_at.toDate(), {
-      addSuffix: true
-    });
+    // const when = dateFns.distanceInWordsToNow(data.created_at.toDate(), {
+    //   addSuffix: true
+    // });
+    const when = data.created_at.toDate();
     const html = `<li class="list-group-item">
         <span class="username">${data.username}</span>
         <span class="message">${data.message}</span>
